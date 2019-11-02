1b3853ec0dde7e3992ac31805f11d7e222a75de6
lqual <53837500+lqual@users.noreply.github.com>
2019-11-02 11:14:07 -0500

    backup

--- downloadFile.R
+++ downloadFile.R
@@ -5,47 +5,58 @@ if(!file.exists("final")) {
         unzip("data.zip")
 }
 
-#splits blogs into manageable chunks
-start <- Sys.time()
-blogs <- read_lines(file("final/en_US/en_US.blogs.txt"), skip = 0, n_max = 400)
-set.seed(485)
-folds<-createFolds(blogs, 20)
-for(i in 1:20) {
-        partition <- blogs[folds[[i]]]
-        name <- paste("split_data/blogs_", i, ".txt", sep = "")
-        write_lines(partition, name)
-}
+blogsPartition <- function() {
+        library(readr)
+        library(caret)
+        #splits blogs into manageable chunks
+        start <- Sys.time()
+        blogs <- read_lines(file("final/en_US/en_US.blogs.txt"), skip = 0)
+        set.seed(485)
+        folds<-createFolds(blogs, 20)
+        for(i in 1:20) {
+                partition <- blogs[folds[[i]]]
+                name <- paste("split_data/blogs_", i, ".txt", sep = "")
+                write_lines(partition, name)
+        }
 finish <- Sys.time()
 print(finish - start)
-
-
-#splits news into manageable chunks
-start <- Sys.time()
-news <- read_lines(file("final/en_US/en_US.news.txt"), skip = 0, n_max = 400)
-set.seed(123)
-folds<-createFolds(news, 20)
-for(i in 1:20) {
-        partition <- news[folds[[i]]]
-        name <- paste("split_data/news_", i, ".txt", sep = "")
-        write_lines(partition, name)
 }
-finish <- Sys.time()
-print(finish - start)
 
 
-#splits twitter into manageable chunks
-start <- Sys.time()
-twitter <- read_lines(file("final/en_US/en_US.twitter.txt"), skip = 0, n_max = 400)
-set.seed(987)
-folds<-createFolds(twitter, 20)
-for(i in 1:20) {
-        partition <- twitter[folds[[i]]]
-        name <- paste("split_data/twitter_", i, ".txt", sep = "")
-        write_lines(partition, name)
+newsPartition <- function() {
+        library(readr)
+        library(caret)
+        #splits news into manageable chunks
+        start <- Sys.time()
+        news <- read_lines(file("final/en_US/en_US.news.txt"), skip = 0)
+        set.seed(123)
+        folds<-createFolds(news, 20)
+        for(i in 1:20) {
+                partition <- news[folds[[i]]]
+                name <- paste("split_data/news_", i, ".txt", sep = "")
+                write_lines(partition, name)
+        }
+        finish <- Sys.time()
+        print(finish - start)
 }
-finish <- Sys.time()
-print(finish - start)
 
+
+twitterPartition <- function() {
+        library(readr)
+        library(caret)
+        #splits twitter into manageable chunks
+        start <- Sys.time()
+        twitter <- read_lines(file("final/en_US/en_US.twitter.txt"), skip = 0)
+        set.seed(987)
+        folds<-createFolds(twitter, 20)
+        for(i in 1:20) {
+                partition <- twitter[folds[[i]]]
+                name <- paste("split_data/twitter_", i, ".txt", sep = "")
+                write_lines(partition, name)
+        }
+        finish <- Sys.time()
+        print(finish - start)
+}
 
 
 
