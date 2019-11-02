a3c58e14ef6aeb0c403702816d0ebe42bc4bc887
lqual <53837500+lqual@users.noreply.github.com>
2019-11-02 11:02:56 -0500

    splitting data

--- downloadFile.R
+++ downloadFile.R
@@ -4,3 +4,55 @@ if(!file.exists("final")) {
         download.file(url, "data.zip")
         unzip("data.zip")
 }
+
+#splits blogs into manageable chunks
+start <- Sys.time()
+blogs <- read_lines(file("final/en_US/en_US.blogs.txt"), skip = 0, n_max = 400)
+set.seed(485)
+folds<-createFolds(blogs, 20)
+for(i in 1:20) {
+        partition <- blogs[folds[[i]]]
+        name <- paste("split_data/blogs_", i, ".txt", sep = "")
+        write_lines(partition, name)
+}
+finish <- Sys.time()
+print(finish - start)
+
+
+#splits news into manageable chunks
+start <- Sys.time()
+news <- read_lines(file("final/en_US/en_US.news.txt"), skip = 0, n_max = 400)
+set.seed(123)
+folds<-createFolds(news, 20)
+for(i in 1:20) {
+        partition <- news[folds[[i]]]
+        name <- paste("split_data/news_", i, ".txt", sep = "")
+        write_lines(partition, name)
+}
+finish <- Sys.time()
+print(finish - start)
+
+
+#splits twitter into manageable chunks
+start <- Sys.time()
+twitter <- read_lines(file("final/en_US/en_US.twitter.txt"), skip = 0, n_max = 400)
+set.seed(987)
+folds<-createFolds(twitter, 20)
+for(i in 1:20) {
+        partition <- twitter[folds[[i]]]
+        name <- paste("split_data/twitter_", i, ".txt", sep = "")
+        write_lines(partition, name)
+}
+finish <- Sys.time()
+print(finish - start)
+
+
+
+
+
+
+
+
+
+
+
--- exploratoryAnalysis.R
+++ exploratoryAnalysis.R
@@ -3,21 +3,17 @@ library(readr)
 library(ngram)
 library(textreg)
 library(tm)
-library(parallel)
-library(doParallel)
+library(caret)
+
 
 #read data into R
-cluster <- makeCluster(detectCores() - 1) # convention to leave 1 core for OS
-registerDoParallel(cluster)
+
 blogs <- read_lines(file("final/en_US/en_US.blogs.txt"), skip = 0)
 news <- read_lines(file("final/en_US/en_US.news.txt"), skip = 0)
 twitter <- read_lines(file("final/en_US/en_US.twitter.txt"), skip = 0)
-stopCluster(cluster)
-registerDoSEQ()
+
 
 #table summation of the data
-cluster <- makeCluster(detectCores() - 1) # convention to leave 1 core for OS
-registerDoParallel(cluster)
 sum_table <- data.frame(matrix(nrow = 3, ncol = 4))
 colnames(sum_table) <- c("File", "Size", "Lines", "Words")
 sum_table$File <- c("Blogs", "News", "Twitter")
@@ -25,18 +21,15 @@ sum_table$Size <- c("255.4 MB", "257.3 MB", "319.0 MB")
 sum_table$Lines <- c(length(blogs), length(news), length(twitter))
 sum_table$Words <- c(wordcount(blogs), wordcount(news), wordcount(twitter))
 sum_table
-stopCluster(cluster)
-registerDoSEQ()
+
 
 #making 10 sets of data
-cluster <- makeCluster(detectCores() - 1) # convention to leave 1 core for OS
-registerDoParallel(cluster)
+
 set.seed(21)
 twitter <- data.frame(twitter = twitter, group = sample(1:10, 1, replace = TRUE)) 
 
 
-stopCluster(cluster)
-registerDoSEQ()
+
 
 
 #build a corpus for analysis
@@ -46,7 +39,11 @@ set.seed(21)
 group <- lapply(full_corpus, function() sample(1:10, 1, replace = TRUE))
 
 
-
+r <- 8
+c <- 10
+m0 <- matrix(0, r, c)
+features<-apply(m0, c(1,2), function(x) sample(c(0,1),1))
+folds<-createFolds(features,4)
 
 
 
