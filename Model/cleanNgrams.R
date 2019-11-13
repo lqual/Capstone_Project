2b4474c55b5a5e86294abd51753ab6d43b8819e9
lqual <53837500+lqual@users.noreply.github.com>
2019-11-13 13:06:31 -0600

    backup

--- Model/cleanNgrams.R
+++ Model/cleanNgrams.R
@@ -5,14 +5,11 @@ cleanNgrams <- function(ngram, numWords, filt = 5) {
         library(tm)
         library(ngram)
         ngram <- as.data.frame(ngram, row.names = NULL)
-        head(rownames(ngram))
         ngram <- data.frame(string = rownames(ngram), num = ngram$X1)
         ngram$string <- as.character(ngram$string)
         ngram <- ngram %>% filter(num > filt) %>% 
                 mutate(next_word = word(string, -1), nchar = 0) %>%
                 mutate(string = word(string, 1, numWords - 1)) 
-        index <- which(duplicated(ngram$string))
-        ngram <- ngram[-index, ]
         id <- c(1:length(ngram$string))
         for(i in id) {
                 char <- nchar(ngram$string[i])
--- Model/textPredictor.R
+++ Model/textPredictor.R
@@ -3,7 +3,8 @@ library(ngram)
 library(tm)
 
 
-text <- "I hope you are"
+#processing text
+text <- "It would mean the"
 length <- wordcount(text)
 text <- VCorpus(VectorSource(text))
 text <- tm_map(text, stripWhitespace)
@@ -13,16 +14,31 @@ text <- tm_map(text, removeNumbers)
 uno <- function(x) unlist(lapply(ngrams(words(x), length), paste, collapse = " "), use.names = FALSE)
 text <- DocumentTermMatrix(text,control=list(tokenize=uno))
 text <- findMostFreqTerms(text, n = 1)
-text[1]
+text <- as.data.frame(text)
+text <- rownames(text)
 
 
+#lookup in file
+numwords <- wordcount(text)
+text5 <- word(text, -4, numwords)
+#filtdf5 <- pentagram %>% filter(nchar == nchar(text5))
+lookup5 <- grep(text5, pentagram$string)
+answer5 <- data.frame(pentagram$next_word[lookup5])
 
+text4 <- word(text, -3, numwords)
+#filtdf4 <- quadragram #%>% filter(nchar == nchar(text4))
+lookup4 <- grep(text4, quadragram$string)
+answer4 <- data.frame(quadragram$next_word[lookup4])
 
+text3 <- word(text, -2, numwords)
+#filtdf3 <- trigram #%>% filter(nchar == nchar(text3))
+lookup3 <- grep(text3, trigram$string)
+answer3 <- data.frame(trigram$next_word[lookup3])
 
-
-
-
-
+text2 <- word(text, -1, numwords)
+#filtdf2 <- bigram #%>% filter(nchar == nchar(text2))
+lookup2 <- grep(text2, bigram$string)
+answer2 <- data.frame(bigram$next_word[lookup2])
 
 
 
